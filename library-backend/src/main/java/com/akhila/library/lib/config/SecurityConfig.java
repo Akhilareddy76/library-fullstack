3b3771100ff5c6acfd65730a073057eb32aac62c
SamathaLingareddy782 <samathapothamsetty65@gmail.com>
2025-11-15 04:36:50 +0530

    config updated

--- library-backend/src/main/java/com/akhila/library/lib/config/SecurityConfig.java
+++ library-backend/src/main/java/com/akhila/library/lib/config/SecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -22,9 +24,8 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(form -> form.disable());
-
         return http.build();
     }
 }
