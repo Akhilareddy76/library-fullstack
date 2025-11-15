a3115bc8b895cae749cd16b2df05d938ce95fada
SamathaLingareddy782 <samathapothamsetty65@gmail.com>
2025-11-16 02:53:42 +0530

    Fix: Tailwind not loading + env added

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import emailjs from "@emailjs/browser";
+import "./index.css";
 
 emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
 
